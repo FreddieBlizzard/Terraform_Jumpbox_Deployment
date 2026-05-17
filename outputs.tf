a5a41e6e7a3b8ef7010efdbc3f05bc96fec331f1
Freddie Plaza <freddieblizzard@gmail.com>
2026-05-17 18:52:12 -0400

    Updated Outputs formatting

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "resource_group_name" {
 
 output "resource_group_id" {
   description = "ID of the deployed resource group"
-  value = azurerm_resource_group.rg1.id
+  value       = azurerm_resource_group.rg1.id
 }
 
 output "jumpbox_public_ip" {
   description = "Pubilc IP of jumpbox reachable via RDP, only for hosts with approved Public IPs"
-  value = azurerm_public_ip.jumpbox_pip.ip_address
+  value       = azurerm_public_ip.jumpbox_pip.ip_address
 }
